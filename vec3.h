2cb8c7fddb0e4f3ab76843f8b1cc51f886670099
LiamHz <liam.hinzman@gmail.com>
2020-02-05 21:53:40 -0500

    Refactored vec3 class

--- vec3.h
+++ vec3.h
@@ -1,57 +1,38 @@
 class vec3 {
 public:
-    float p[3];
-    vec3(float p0, float p1, float p2) { p[0] = p0; p[1] = p1; p[2] = p2; };
-    inline float x() const { return p[0]; };
-    inline float y() const { return p[1]; };
-    inline float z() const { return p[2]; };
+    int x, y, z;
+    vec3(float p0, float p1, float p2) { x = p0; y = p1; x = p2; };
+    // inline float x() const { return p[0]; };
+    // inline float y() const { return p[1]; };
+    // inline float z() const { return p[2]; };
 
-    inline vec3 operator-() const { return vec3(-p[0], -p[1], -p[2]); }
-    inline float operator[](int i) const { return p[i]; }
+    inline float operator [](int i) const {
+        if (i == 0) return x;
+        else if (i == 1) return y;
+        else if (i == 2) return z;
+    }
+
+    inline vec3 operator -() const { return vec3(-x, -y, -z); }
+    inline vec3 operator +(const vec3 &v) const { return vec3(x+v.x, y+v.y, z+v.z); }
+    inline vec3 operator -(const vec3 &v) const { return vec3(x-v.x, y-v.y, z-v.z); }
+    inline vec3 operator *(const float f) const { return vec3(x*f, y*f, z*f); }
+    inline vec3 operator /(const float f) const { return vec3(x/f, y/f, z/f); }
+
+    // Hadarmard product
+    inline vec3 operator *(const vec3 &v) const { return vec3(x*v.x, y*v.y, z*v.z); }
 };
 
-inline std::istream& operator>>(std::istream &is, vec3 &v) {
-    is >> v.p[0] >> v.p[1] >> v.p[2];
-    return is;
+inline float dot(const vec3 &v1, const vec3 &v2) {
+    return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;
 }
 
+inline std::istream& operator>>(std::istream &is, vec3 &v) {
+    is >> v.x >> v.y >> v.z;
+    return is;
+}
 inline std::ostream& operator<<(std::ostream &os, vec3 &v) {
-    os << v[0] << " " << v[1] << " " << v[2];
+    os << v.x << " " << v.y << " " << v.z;
     return os;
 }
 
-inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
-    return vec3(v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]);
-}
 
-inline vec3 operator-(const vec3 &v1, const vec3 &v2) {
-    return vec3(v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]);
-}
-
-// Hadamard product
-inline vec3 operator*(const vec3 &v1, const vec3 &v2) {
-    return vec3(v1[0] * v2[0], v1[1] * v2[1], v1[2] * v2[2]);
-}
-
-// Hadamard division
-inline vec3 operator/(const vec3 &v1, const vec3 &v2) {
-    return vec3(v1[0] / v2[0], v1[1] / v2[1], v1[2] / v2[2]);
-}
-
-inline vec3 operator*(const float t, const vec3 &v) {
-    return vec3(t*v[0], t*v[1], t*v[2]);
-}
-
-inline vec3 operator*(const vec3 &v, const float t) {
-    return vec3(t*v[0], t*v[1], t*v[2]);
-}
-
-inline vec3 operator/(const vec3 &v, const float t) {
-    return vec3(v[0]/t, v[1]/t, v[2]/t);
-}
-
-inline float dot(const vec3 &v1, const vec3 &v2) {
-    return v1[0] + v2[0]
-         + v1[1] + v2[1]
-         + v1[2] + v2[2];
-}
