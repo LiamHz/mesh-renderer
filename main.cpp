be5943435871f85bfaa392a241dff78a0c1757d8
LiamHz <liam.hinzman@gmail.com>
2020-02-06 14:16:04 -0500

    Implemented scanline algorithm for filled triangle

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 // DONE Implement Bresenham's line drawing algorithm
 // DONE Implement vec2 library
 // DONE Implement triangle drawing
-// TODO Implement Bresenham's filled triangle drawing algorithm
-// TODO Implement mesh rendering
+// DONE Implement scanline algorithm for filled triangles
+// TODO Implement obj mesh loading
 
 #include <iostream>
 #include <math.h>
@@ -57,21 +57,47 @@ void line(vec2 p0, vec2 p1, TGAImage &img, TGAColor color) {
     }
 }
 
-void triangle(vec2 t0, vec2 t1, vec2 t2, TGAImage &img, TGAColor color) {
-    line(t0, t1, img, color);
-    line(t1, t2, img, color);
-    line(t2, t0, img, color);
+void triangle(vec2 v0, vec2 v1, vec2 v2, TGAImage &img, TGAColor color) {
+    // Bubble sort verticies acending by y-value
+    if (v0.y > v1.y) std::swap(v0, v1);
+    if (v1.y > v2.y) std::swap(v1, v2);
+    if (v0.y > v1.y) std::swap(v0, v1);
+
+    // Marks partition point to create two flat sided triangles
+    // Use intercept theorem and similar triangles to find v3.x
+    int v3_x = static_cast<int>(v2.x + ((v1.y-v2.y)/(v0.y-v2.y) * (v0.x-v2.x)));
+    vec2 v3(v3_x, v1.y);
+
+    int total_height = v2.y - v0.y;
+
+    for (int y=0; y<total_height; y++) {
+        // Use conditionals to accound for both triangle segments
+        bool second_half = y > v3.y - v0.y || v3.y == v0.y;
+        int segment_height = second_half ? v2.y - v1.y : v1.y - v0.y;
+
+        // Get the position of the left and right edge of the triangle at ypos
+        float alpha = (float) y / total_height;
+        float beta  = (float)(y - (second_half ? v1.y - v0.y : 0))/segment_height;
+        vec2 left =                v0 + (v2 - v0) * alpha;
+        vec2 right = second_half ? v1 + (v2 - v1) * beta : v0 + (v1 - v0) * beta;
+        if (left.x > right.x) std::swap(left, right);
+
+        // Line fill from left to right edge
+        for (int x=left.x; x <= right.x; x++) {
+            img.set(x, v0.y+y, color);
+        }
+    }
 }
 
 int main() {
     TGAImage img(width, height, TGAImage::RGB);
 
-    vec2 t0[3] = {vec2(10, 70),   vec2(50, 160),  vec2(70, 80)};
-    vec2 t1[3] = {vec2(180, 50),  vec2(150, 1),   vec2(70, 180)};
-    vec2 t2[3] = {vec2(180, 150), vec2(120, 160), vec2(130, 180)};
-    triangle(t0[0], t0[1], t0[2], img, red);
-    triangle(t1[0], t1[1], t1[2], img, white);
-    triangle(t2[0], t2[1], t2[2], img, green);
+    vec2 v0[3] = {vec2(10, 70),   vec2(50, 160),  vec2(70, 80)};
+    vec2 v1[3] = {vec2(180, 50),  vec2(150, 1),   vec2(70, 180)};
+    vec2 v2[3] = {vec2(180, 150), vec2(120, 160), vec2(130, 180)};
+    triangle(v0[0], v0[1], v0[2], img, red);
+    triangle(v1[0], v1[1], v1[2], img, white);
+    triangle(v2[0], v2[1], v2[2], img, green);
 
     // Set origin at bottom left instead of top left of image
     img.flip_vertically();
